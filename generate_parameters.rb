fca310ac7fdbe8dbbaa74d1cf3deab1640b8bec4
almeidabbm <bruno@proctorexam.com>
2019-02-04 09:12:38 +0000

    Updates generate parameters

--- generate_parameters.rb
+++ generate_parameters.rb
@@ -14,13 +14,14 @@ require 'json'
 
 # Order the nested params
 def generate_params_with_signature(params, user_secret)
-  params[:nonce] = Time.now.to_i.to_s
+  params[:nonce] = Time.now.to_i
   params[:timestamp] = (Time.now.to_f*1000).to_i
   signature = OpenSSL::HMAC.hexdigest(
                   OpenSSL::Digest.new('sha256'),
                   user_secret,
                   params.keys.sort.map{|key|
                   "#{key}=#{params[key]}"}.join("?"))
+  puts params.keys.sort.map{|key| "#{key}=#{params[key]}"}.join("?")
   params[:signature] = signature
   return params
 end
