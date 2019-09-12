658809a16744b7390693d61700689705eba66f4a
almeidabbm <bruno@proctorexam.com>
2019-09-12 11:39:55 +0100

    Fix generate params, add api_requests for create exam, add_student and post create student

---
+++ api_requests.rb
@@ -0,0 +1,52 @@
+require "./generate_parameters.rb"
+require "net/http"
+require "uri"
+require "json"
+
+# Domain to where the request is done
+DOMAIN = "https://localhost.proctorexam.com:3001"
+
+# Key used to sign the request
+SECRET_KEY = "QKyW3Z7_gQfK-tmJtDF8pAH2wbryLvYgUrA8iUaPBO8"
+
+# Key sent in the header to identify the user
+API_TOKEN = "_WhA0nyQoLCpcP0qPnufNA"
+
+# HTTP Header
+HTTP_HEADER = {
+  "Content-Type" => "application/json",
+  "Authorization" => "Token token=#{API_TOKEN}",
+  "Accept" => "application/vnd.procwise.v3"
+}
+
+# POST /exams
+def create_exam(params)
+  post("/api/v3/exams", params)
+end
+
+# POST /exams/:id/add_student
+def add_student(params)
+  post("/api/v3/exams/#{params[:id]}/add_student", params)
+end
+
+# POST /exams/:exam_id/student_sessions
+def create_student(params)
+  post("/api/v3/exams/#{params[:exam_id]}/student_sessions", params)
+end
+
+def post(url_path, params)
+  uri = URI.parse(DOMAIN + url_path)
+  signed_params = generate_params_with_signature(params, SECRET_KEY)
+
+  # Request objects
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  puts HTTP_HEADER
+  request = Net::HTTP::Post.new(uri.request_uri, HTTP_HEADER)
+  request.body = signed_params.to_json
+
+  # Send the request
+  response = http.request(request)
+  puts response.body
+end
--- generate_parameters.rb
+++ generate_parameters.rb
@@ -7,21 +7,24 @@ require 'json'
 # Create a hash that contains all the parameters that you want to send
 # Call generate_params_with_signature with the hash as the first argument and the user_secret as the second
 #
-# Note: timestamp and nonce are automatically generated and added to params:
+# Note 1: The ID's in the endpoint to be requested should be in the params to be signed otherwise
+# the signature will be incorrect
+#
+# Note 2: timestamp and nonce are automatically generated and added to params:
 # timestamp: (Time.now.to_f*1000).to_i\nnonce: Time.now.to_i
 # ------------------------------------//------------------------------------
 #
 
-# Order the nested params
+# Sign the params
 def generate_params_with_signature(params, user_secret)
-  params[:nonce] = Time.now.to_i
-  params[:timestamp] = (Time.now.to_f*1000).to_i
+  params_copy = params.clone
+  params_copy[:nonce] = Time.now.to_i
+  params_copy[:timestamp] = (Time.now.to_f*1000).to_i
   signature = OpenSSL::HMAC.hexdigest(
                   OpenSSL::Digest.new('sha256'),
                   user_secret,
-                  params.keys.sort.map{|key|
-                  "#{key}=#{params[key]}"}.join("?"))
-  puts params.keys.sort.map{|key| "#{key}=#{params[key]}"}.join("?")
-  params[:signature] = signature
-  return params
+                  params_copy.keys.sort.map{|key|
+                  "#{key}=#{params_copy[key]}"}.join("?"))
+  params_copy[:signature] = signature
+  return params_copy
 end
