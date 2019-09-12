91e7445082a78aca4c4d98d527bb736d0f6eb966
almeidabbm <bruno@proctorexam.com>
2019-09-12 12:37:37 +0100

    Add async requests

--- api_requests.rb
+++ api_requests.rb
@@ -28,13 +28,13 @@ HTTP_HEADER = {
 ######################
 
 # POST /exams
-def create_exam(params)
-  post("/api/v3/exams", params)
+def create_exam(params, number_of_requests=1)
+  post("/api/v3/exams", params, number_of_requests)
 end
 
 # POST /exams/:id/add_student
-def add_student(params)
-  post("/api/v3/exams/#{params[:id]}/add_student", params)
+def add_student(params, number_of_requests=1)
+  post("/api/v3/exams/#{params[:id]}/add_student", params, number_of_requests)
 end
 
 ################################
@@ -42,15 +42,15 @@ end
 ################################
 
 # POST /exams/:exam_id/student_sessions
-def create_student_session(params)
-  post("/api/v3/exams/#{params[:exam_id]}/student_sessions", params)
+def create_student_session(params, number_of_requests=1)
+  post("/api/v3/exams/#{params[:exam_id]}/student_sessions", params, number_of_requests)
 end
 
 #######################
 # Auxiliary Functions #
 #######################
 
-def post(url_path, params)
+def post(url_path, params, number_of_requests)
   uri = URI.parse(DOMAIN + url_path)
   signed_params = generate_params_with_signature(params, SECRET_KEY)
 
@@ -58,11 +58,18 @@ def post(url_path, params)
   http = Net::HTTP.new(uri.host, uri.port)
   http.use_ssl = true
   http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-  puts HTTP_HEADER
   request = Net::HTTP::Post.new(uri.request_uri, HTTP_HEADER)
   request.body = signed_params.to_json
 
-  # Send the request
-  response = http.request(request)
-  puts response.body
+  async_request(number_of_requests, http, request)
+end
+
+def async_request(number_of_requests, http, request)
+  threads = number_of_requests.times.map do |i|
+    Thread.new {
+      response = http.request(request)
+    }
+  end
+
+  threads.each &:join
 end
